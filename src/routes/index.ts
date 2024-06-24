a29a2188a6006794d3b8754ed5d7586986cfbe61
Piotr Debski <piotr.debski@gmail.com>
2024-06-25 00:24:35 +0200

    Swagger + Planets

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,19 @@
 import { Request, Response, Router } from 'express'
+import swaggerUIExpress from 'swagger-ui-express'
+import swaggerDocument from '../../swagger.json'
 
-export default Router().get('/', async (req: Request, res: Response) => {
+const router = Router()
+
+router.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+if (process.env.NODE_ENV !== 'production') {
+  router.use('/docs', swaggerUIExpress.serve)
+  router.get('/docs', swaggerUIExpress.setup(swaggerDocument))
+  router.get('/docs/swagger.json', (req: Request, res: Response) =>
+    res.json(swaggerDocument)
+  )
+}
+
+export default router
