78ff77d8bf18450c196e21c62ea782b96ef8e852
Piotr Debski <piotr.debski@gmail.com>
2024-06-25 11:57:15 +0200

    Chabge db port

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, NextFunction, Request, Response } from 'express'
+import express, { Express } from 'express'
 import dotenv from 'dotenv'
 import router from './middlewares/router'
 import bodyParser from 'body-parser'
